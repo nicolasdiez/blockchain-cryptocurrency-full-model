5afc3edf3b203bba3501ae59acc40084ff2d9f0d
Nico <nicolas439@gmail.com>
2022-06-13 21:04:54 +0200

    block module created

---
+++ block.py
@@ -0,0 +1,22 @@
+class Block:
+    """
+    Block: unit of storage in a blockchain network that supports cryptocurrency.
+    A Block contains 1 or more transactions.
+    """
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):   # __repr__ is a special method used to represent a class's objects as a string
+        return f'Block - data: {self.data}'  # __repr__ is called by the repr() built-in function
+
+
+def main():  # including debug code here, so it only executes when directly call this file from cli
+    block = Block('foo')
+    print(block)
+
+    print(f'block.py __name__: {__name__}')
+
+
+if __name__ == '__main__':
+    main()
+
--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,4 @@
-class Block:
-    """
-    Block: unit of storage in a blockchain network that supports cryptocurrency.
-    A Block contains 1 or more transactions.
-    A transaction is a transfer of cryptocurrency between 2 participants
-    lalalalala
-    """
-    def __init__(self, data):
-        self.data = data
-
-    def __repr__(self):   # __repr__ is a special method used to represent a class's objects as a string
-        return f'Block - data: {self.data}'  # __repr__ is called by the repr() built-in function
+from block import Block
 
 
 class Blockchain:
@@ -27,8 +16,14 @@ class Blockchain:
         return f'Blockchain: {self.chain}'
 
 
-blockchain = Blockchain()
-blockchain.add_block('one')
-blockchain.add_block('two')
+def main():  # including debug code here, so it only executes when directly call this file from cli
+    blockchain = Blockchain()
+    blockchain.add_block('one')
+    blockchain.add_block('two')
 
-print(blockchain)
+    print(blockchain)
+    print(f'blockchain.py __name__: {__name__}')
+
+
+if __name__ == '__main__':
+    main()
