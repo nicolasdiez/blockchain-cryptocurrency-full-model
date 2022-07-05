5686ec311f64ae266eb3515a2b1eee3c4062860f
Nico <nicolas439@gmail.com>
2022-07-05 18:38:24 +0200

    finished methods to update and validate transactions + unit testing

--- backend/tests/wallet/test_transaction.py
+++ backend/tests/wallet/test_transaction.py
@@ -5,7 +5,7 @@ import pytest
 
 def test_transaction():
     sender_wallet = Wallet()
-    recipient_address = 'recipient'
+    recipient_address = 'recipient-test-address'
     amount = 15
     transaction = Transaction(sender_wallet, recipient_address, amount)
 
@@ -23,4 +23,95 @@ def test_transaction():
 def test_transaction_balance_exceeded():
     # expecting an exception to be raised when creating the Transaction (20000 > STARTING_BALANCE)
     with pytest.raises(Exception, match="Amount exceeds sender's balance"):
-        Transaction(Wallet(), 'recipient', 20000)
+        sender_wallet = Wallet()
+        recipient_address = 'recipient-test-address'
+        amount = 20000
+        Transaction(sender_wallet, recipient_address, amount)
+
+
+def test_update_transaction_balance_exceeded():
+    sender_wallet = Wallet()
+    recipient_address = 'recipient-test-address'
+    amount = 15
+    transaction = Transaction(sender_wallet, recipient_address, amount)
+
+    # expecting an exception to be raised when creating the Transaction (20000 > STARTING_BALANCE)
+    with pytest.raises(Exception, match="Amount exceeds sender's balance"):
+        updated_recipient_address = 'updated-recipient-test-address'
+        updated_amount = 20000
+        transaction.update_transaction(sender_wallet, updated_recipient_address, updated_amount)
+
+
+def test_update_transaction_successfully_new_recipient():
+    # generate a 1st transaction to one recipient
+    sender_wallet = Wallet()
+    recipient_address = 'recipient-test-address'
+    amount = 15
+    transaction = Transaction(sender_wallet, recipient_address, amount)
+
+    # update the current transaction to include a 2nd recipient
+    next_recipient_address = 'next-recipient-test-address'
+    next_amount = 25
+    transaction.update_transaction(sender_wallet, next_recipient_address, next_amount)
+
+    assert transaction.output[next_recipient_address] == next_amount
+    assert transaction.output[sender_wallet.address] == sender_wallet.balance - amount - next_amount
+
+    # True is excepted to be returned by verify_signature()
+    assert Wallet.verify_signature(transaction.input['public_key'], transaction.output, transaction.input['signature'])
+
+
+def test_update_transaction_successfully_same_recipient():
+    # generate a 1st transaction to one recipient
+    sender_wallet = Wallet()
+    recipient_address = 'recipient-test-address'
+    amount = 15
+    transaction = Transaction(sender_wallet, recipient_address, amount)
+
+    # add amount to the same 1st recipient
+    new_amount = 75
+    transaction.update_transaction(sender_wallet, recipient_address, new_amount)
+
+    assert transaction.output[recipient_address] == amount + new_amount
+    assert transaction.output[sender_wallet.address] == sender_wallet.balance - amount - new_amount
+
+    # True is excepted to be returned by verify_signature()
+    assert Wallet.verify_signature(transaction.input['public_key'], transaction.output, transaction.input['signature'])
+
+
+def test_validate_transaction_transaction_ok():
+    sender_wallet = Wallet()
+    recipient_address = 'recipient-test-address'
+    amount = 15
+    Transaction.is_valid_transaction(Transaction(sender_wallet, recipient_address, amount))
+
+
+def test_validate_transaction_invalid_output():
+    sender_wallet = Wallet()
+    recipient_address = 'recipient-test-address'
+    amount = 15
+    transaction = Transaction(sender_wallet, recipient_address, amount)
+
+    # tamper the output balance for the sender wallet
+    transaction.output[sender_wallet.address] = 5000
+
+    # expected an exception to be raised here due to the tampering of the output sender's wallet balance
+    with pytest.raises(Exception, match='Error in the output transaction balance'):
+        Transaction.is_valid_transaction(transaction)
+
+
+def test_validate_transaction_invalid_signature():
+    sender_wallet = Wallet()
+    recipient_address = 'recipient-test-address'
+    amount = 15
+    transaction = Transaction(sender_wallet, recipient_address, amount)
+
+    # sign the same output data but with a different private key from a new Wallet
+    new_signature = Wallet().sign(transaction.output)
+
+    transaction.input['signature'] = new_signature
+
+    # expected an exception to be raised here due to the tampering of the signature
+    # reminder --> signature = data + private_key
+    with pytest.raises(Exception, match='Error in transaction signature'):
+        Transaction.is_valid_transaction(transaction)
--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -7,6 +7,9 @@ from backend.wallet.wallet import Wallet
 class Transaction:
     """
     Written proof of a currency exchange between one sender and one or more recipients
+    Considerations of a Transaction:
+    - Only 1 transaction per sender per block
+    - A transaction can contain multiple recipients
     """
 
     def __init__(self, sender_wallet, recipient_address, amount):
@@ -48,6 +51,41 @@ class Transaction:
             'public_key': sender_wallet.public_key,
             'signature': sender_wallet.sign(output_balance)
         }
+
+    def update_transaction(self, sender_wallet, recipient_address, amount):
+        """
+        Override an existing transaction for an existing or a new recipient
+        """
+
+        if amount > self.output[sender_wallet.address]:
+            raise Exception("Amount exceeds sender's balance")
+
+        # if the recipient is an existing one
+        if recipient_address in self.output:
+            self.output[recipient_address] = self.output[recipient_address] + amount
+        # if the recipient is a new one
+        else:
+            self.output[recipient_address] = amount
+
+        # update the sender´s wallet balance in the output balance structure
+        self.output[sender_wallet.address] = self.output[sender_wallet.address] - amount
+
+        # because the output final balance has changed due to the update, the transaction has to be signed again
+        self.input = self.generate_transaction(sender_wallet, self.output)
+
+    @staticmethod
+    def is_valid_transaction(transaction):
+        """
+        Verifies is a transaction is correct in terms of structure.
+        """
+        output_total_balance = sum(transaction.output.values())
+
+        if transaction.input['amount'] != output_total_balance:
+            raise Exception('Error in the output transaction balance')
+
+        if not Wallet.verify_signature(transaction.input['public_key'], transaction.output,
+                                       transaction.input['signature']):
+            raise Exception('Error in transaction signature')
 
 
 def main():
